628c7b5e33db92dbc49304ef6126b15726f1231e
SolomonChidera <solochidera10@gmail.com>
2023-08-18 00:26:05 +0100

    [Understanding call by value in function]

---
+++ 0x02Function/callbyvalue.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+void modifyValue(int a)
+{
+	a = a * 2;
+	printf("Inside Function: a is %d\n", a);
+}
+
+int main(void)
+{
+	int num = 5;
+	modifyValue(num);
+	printf("Outside Function: num is %d\n");
+
+	return (0);
+}
