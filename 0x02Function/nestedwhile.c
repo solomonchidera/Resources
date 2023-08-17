e67a503a418a7873e13e922cbee3fefb263fa7e8
SolomonChidera <solochidera10@gmail.com>
2023-08-17 12:37:46 +0100

    [Nested while loop in C to print 2 2 2 by 5 lines]

--- 0x02Function/nestedwhile.c
+++ 0x02Function/nestedwhile.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * FileName:
+ * FileName:nestedwhile.c
  * Author: SolomonChidera
  * What does it do: Nested while loop in C.
  */
@@ -10,8 +10,15 @@ int main(void)
 {
 	int a = 0, b;
 
-	while (i < 5)
+	while (a < 5)
 	{
-		puchar()
+		while (b < 3)
+		{
+			printf("2 ");
+			b++;
+		}
+
+		putchar(10);
+		a++;
 	}
 }
