e37dc7aabc04aae9165d4e69d5625444bb979a49
SolomonChidera <solochidera10@gmail.com>
2023-08-17 22:04:42 +0100

    [A main function that calls the function 'max'

---
+++ 0x02Function/callmax.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+
+int max(int one, int two);
+
+int main(void)
+{
+	int first, seccond;
+	
+	first = 40;
+
+	second = 20;
+
+	int bigger;
+
+	bigger = max(first, second);
+
+	return (0);
+}
