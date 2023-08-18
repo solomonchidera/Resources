a710203665d6b5d31ec1a24c0cfc86491fbe3952
SolomonChidera <solochidera10@gmail.com>
2023-08-18 12:12:39 +0100

    function that checks for uppercase character

---
+++ Function/checkupper.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+
+int _islower(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+		return (0);
+
+	return (0);
+}
+
+int main(void)
+{
+	int i;
+	i = _islower('F');
+	printf("%d", i);
+	i = _islower('f');
+	printf("%d", i);
+	i = _islower('Y');
+	printf("%d", i);
+	i = _islower('H');
+	printf("%d", i);
+	i = _islower('f');
+	printf("%d", i);
+
+	return (0);
+}
