9c636edca3cdbad29bd014deda4e7842657e32b2
SolomonChidera <solochidera10@gmail.com>
2023-08-19 16:04:57 +0100

    function that prints the sign of a number

---
+++ Function/5-sign.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		return (1);
+		_putchar(43);
+	}
+	else if (n == 0)
+	{
+		return (0);
+		_putchar(48);
+	}
+	else if (n < 0)
+	{
+		return (-1);
+		_putchar(45);
+	}
+}
