dce4e90cc436c08f157ba91b2b22affa55a0beaa
SolomonChidera <solochidera10@gmail.com>
2023-08-20 13:01:49 +0100

    Nested while loop in C

---
+++ Function/nestedwhile.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * FileName:nestedwhile.c
+ * Author: SolomonChidera
+ * What does it do: Nested while loop in C.
+ */
+
+int main(void)
+{
+	int a = 0, b, c;
+
+	while (a < 5)
+	{
+		b = 0;
+		while (b < 5)
+		{
+			c = 0;
+
+			while (c < 5)
+			{
+				printf(" 5 ");
+
+			printf("2 ");
+			b++;
+			}
+		}
+
+		putchar(10);
+		a = a + 1;
+	}
+	return(0);
+}
