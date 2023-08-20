f4a5f4c625eb09933abce3063fac204f24645558
SolomonChidera <solochidera10@gmail.com>
2023-08-20 13:38:28 +0100

    test case

---
+++ Function/6-abs.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int r;
+
+	r = _abs(-1);
+	printf("%d\n", r);
+	r = _abs(-11);
+	printf("%d\n", r);
+	r = _abs(7);
+	printf("%d\n", r);
+	r = _abs(1);
+	printf("%d\n", r);
+	r = _abs(-98);
+	printf("%d\n", r);
+	r = _abs(-10);
+	printf("%d\n", r);
+	return (0);
+
+}
