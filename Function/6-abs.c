61deada28800521f49921a4e5e8ce616ee79334a
SolomonChidera <solochidera10@gmail.com>
2023-08-20 13:50:40 +0100

    Function that prints the absolute value of an integer

--- Function/6-abs.c
+++ Function/6-abs.c
@@ -1,26 +1,9 @@
 #include "main.h"
 
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
+int _abs(int)
 {
-	int r;
-
-	r = _abs(-1);
-	printf("%d\n", r);
-	r = _abs(-11);
-	printf("%d\n", r);
-	r = _abs(7);
-	printf("%d\n", r);
-	r = _abs(1);
-	printf("%d\n", r);
-	r = _abs(-98);
-	printf("%d\n", r);
-	r = _abs(-10);
-	printf("%d\n", r);
-	return (0);
-
+	if (a < 0)
+		return (-a);
+	else
+		return (a);
 }
