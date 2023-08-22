9de2c6a1c9b9e64c882aa241c3740fa488f21e2b
SolomonChidera <solochidera10@gmail.com>
2023-08-22 04:04:16 +0100

    sizeof

--- Pointer/sizeof.c
+++ Pointer/sizeof.c
@@ -4,11 +4,11 @@ int main(void)
 {
 	int n;
 
-	printf("Size of char on my computer is %lu bytes ", sizeof(char));
-	printf("size of int on my computer is %lu bytes", sizeof(int));
-	printf("size of float on my computer is %lu bytes", sizeof(float));
-	printf("size of double on my computer is %lu bytes", sizeof(double));
-	printf("size of my variable 'n' is %lu bytes", sizeof(n));
+	printf("Size of char on my computer is %lu bytes \n", sizeof(char));
+	printf("size of int on my computer is %lu bytes \n", sizeof(int));
+	printf("size of float on my computer is %lu bytes \n", sizeof(float));
+	printf("size of double on my computer is %lu bytes \n", sizeof(double));
+	printf("size of my variable 'n' is %lu bytes \n", sizeof(n));
 
 	return (0);
 }
