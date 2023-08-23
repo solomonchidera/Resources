843adea96a83c9c9fc5a6e213e7cbdff7bc9aa3f
SolomonChidera <solochidera10@gmail.com>
2023-08-23 04:49:40 +0100

    derefencing pointers

--- Pointer/5-main.c
+++ Pointer/5-main.c
@@ -6,12 +6,21 @@ int main(void)
 	int *p;
 
 	n = 98;
+	
+	int a = &n;
+	printf("The address of n: %p\n", a);
+
 	p = &n;
+
 	printf("Value of 'n': %d\n", n);
 	printf("Address of 'n': %p\n", &n);
+
 	printf("\n---------------------------\n");
+
 	printf("The value of *p is: %p\n", p);
+
 	*p = 402;
+
 	printf("Value of 'n': %d\n", n);
 	return (0);
 }
