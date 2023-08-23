2f1ae9d09b82aa53added5a01504fdc4ab4af33a
SolomonChidera <solochidera10@gmail.com>
2023-08-23 04:11:47 +0100

    printing the address of a pointer

---
+++ Pointer/4-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+int main(void)
+{
+	int n;
+	int *p;
+
+	n = 98;
+	p = &n;
+
+	printf("Address of n = %p\n", n);
+	printf("value of our pointer is %p\n", p);
+
+	return (0);
+}
