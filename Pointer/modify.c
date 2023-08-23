91c03b1715106833e0c1a7da35ddb99118be35d2
SolomonChidera <solochidera10@gmail.com>
2023-08-23 05:27:37 +0100

    Functions parameters are passed by value

---
+++ Pointer/modify.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * modify_my_param - this function does not modify m
+ * @m: a useless integer
+ *
+ * Returns: nothing
+ */
+
+void modify_my_param(int m)
+{
+	m = 402;
+}
+
+/**
+ * main - parameters are passed by value
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int n;
+
+	n = 98;
+	modify_my_param(n);
+	return (0);
+}
