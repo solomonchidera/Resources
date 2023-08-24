40a17b3dc8e03b2d49b9075a22bccaa67e9a5987
SolomonChidera <solochidera10@gmail.com>
2023-08-24 16:56:29 +0100

    pointer arithmetic

---
+++ Pointer/21-main.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+int main(void)
+{
+	int a[5];
+	int *p;
+	int *p2;
+
+	*a = 98;
+	printf("*a: [%d]\n", *a);
+	*(a + 1) = 198;
+	printf("*(a + 1) [%d]\n", *(a + 1));
+	*(a + 2) = 298;
+	a[3] = 398;
+	*(a + 4) = 498;
+	p = a + 1;
+	*p = 98;
+	p2 = a + 3;
+	*p2 = *p + 1337;
+	return (0);
+}
