4b10e3b735d1210a4066ff59710dfcd281b93226
SolomonChidera <solochidera10@gmail.com>
2023-08-24 10:28:08 +0100

    Illustrating pointer arithmetic

--- Pointer/11-main.c
+++ Pointer/11-main.c
@@ -9,9 +9,18 @@ int main(void)
 	*(a + 2) = 298;
 	*(a + 3) = 398;
 	*(a + 4) = 498;
-	printf("Value of a[0]: %d", *a);
-	printf("value of a[1]: %d", *(a + 1));
-	printf("Value of a[4]: %d", a[4]);
+	printf("Value of a[0]: %d\n", *a);
+	printf("value of a[1]: %d\n", *(a + 1));
+	printf("Value of a[4]: %d\n", a[4]);
+	printf("Value of a[4]: %d\n", *(a + 4));
+	printf("Value of a[5]: %d\n", *(a + 2));
+	printf("Value of a[3]: %d\n", *(a + 3));
+	printf("-----------------------------\n");
+	printf("Value of 'a' (also address of a[0]): %p\n", a);
+	printf("Address of 'a[1]': %p", (a + 1));
+	printf("Address of 'a[1]': %p", &a[1]);
+	printf("Address of 'a[1]': %p", &(*(a + 1)));
+
 	return (0);
 
 }
