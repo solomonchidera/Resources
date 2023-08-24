5d041ddf3e709de9f725288769796c5eb4f10d02
SolomonChidera <solochidera10@gmail.com>
2023-08-24 07:58:37 +0100

    Intro to array

--- Pointer/9-main.c
+++ Pointer/9-main.c
@@ -14,6 +14,12 @@ int main(void)
 	printf("Value of the third index which is a[2] is %d\n", a[2]);
 	printf("Value of the fourth index which is a[3] is %d\n", a[3]);
 	printf("Value of the last index which is a[4] is %d\n", a[4]);
+	printf("\n-------------------------------------------------------------\n");
+	printf("Address of the first a[0]: [%p]\n", &a[0]);
+	printf("Address of the second a[1]: [%p]\n", &a[1]);
+	printf("Address of the third a[2]: [%p]\n", &a[2]);
+	printf("Address of the fourth a[3]: [%p]\n", &a[3]);
+	printf("Address of the fifth a[4]: [%p]\n", &a[4]);
 
 	return (0);
 }
