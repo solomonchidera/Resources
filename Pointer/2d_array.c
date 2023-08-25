88914a7a184984775c412cbdc662f61695664717
SolomonChidera <solochidera10@gmail.com>
2023-08-25 12:36:33 +0100

    2d array

---
+++ Pointer/2d_array.c
@@ -0,0 +1,8 @@
+#include "main.h"
+
+int main(void)
+{
+	int r_c[5][4] = {{0,1,2,3},{4,5,6,7},{8,9,10,11}};
+	printf("Accessing 2d array is simple, take a look %d, %d, %d, %d", *r_c);
+	return (0);
+}
