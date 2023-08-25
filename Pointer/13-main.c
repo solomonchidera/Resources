22a196758c8990f38a75cc3e65e5837defef58a5
SolomonChidera <solochidera10@gmail.com>
2023-08-25 08:39:26 +0100

    The last element of a string is usually a null character

--- Pointer/13-main.c
+++ Pointer/13-main.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include "string.h"
 
 int main(void)
 {
@@ -7,5 +8,6 @@ int main(void)
 	printf("%s\n", a);
 	printf("Value of the last character of a is: %d\n", a[7]);
 	printf("value of \"school\":%p\n", "school");
+	printf("Address of a[4]: %p\n", a[4]);
 	return (0);
 }
