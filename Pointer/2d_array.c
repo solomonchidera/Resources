cf984fcb1fe8f72669f074fd1b1c2bfd4f61e45e
SolomonChidera <solochidera10@gmail.com>
2023-08-25 12:49:59 +0100

    2d array

--- Pointer/2d_array.c
+++ Pointer/2d_array.c
@@ -3,6 +3,16 @@
 int main(void)
 {
 	int r_c[5][4] = {{0,1,2,3},{4,5,6,7},{8,9,10,11}};
-	printf("Accessing 2d array is simple, take a look %d, %d, %d", *r_c);
+	int row;
+	int column;
+
+	for (row = 0; row < 5; row++)
+	{
+		for (column = 0; column <= 4; column++)
+		{
+			printf("Element at r_c[%d][%d]: ", row, column);
+			printf("%d\n", r_c[row][column]);
+		}
+	}
 	return (0);
 }
