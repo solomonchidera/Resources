7f67e5ee353a25c33cff9706ef7c79685b2d01b6
SolomonChidera <solochidera10@gmail.com>
2023-08-25 10:48:10 +0100

    still on array

--- Pointer/array1.c
+++ Pointer/array1.c
@@ -3,7 +3,7 @@
 int main(void)
 {
 	double salary[];
-	scanf("%d", &salary);
+	scanf("%lf", &salary);
 	printf("The first number is %lf and the second number is: %lf", *salary, *(salary + 1));
 	return (0);
 }
