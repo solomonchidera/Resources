4d8ee1f9f10b47c0d9d6b6c8c5889f3f1239290f
SolomonChidera <solochidera10@gmail.com>
2023-08-25 10:23:02 +0100

    fgets() function which reads a specified number of characters.

--- Pointer/gets.c
+++ Pointer/gets.c
@@ -4,7 +4,7 @@ int main(void)
 {
 	char fullName[25];
 	printf("please enter your fullname\n");
-	gets(fullName);
+	fget(fullName, 25, stdin);
 	printf("My full name is %s", fullName);
 	return (0);
 }
