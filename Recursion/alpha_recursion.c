ee450af9875d183f0ace9f7c68d025ea03bf6ef7
SolomonChidera <solochidera10@gmail.com>
2023-08-30 20:29:39 +0100

    function that prints the alphabet using recursion

---
+++ Recursion/alpha_recursion.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <unistd.h>
+
+void print_char(char c)
+{
+	write(1 &c, 1);
+}
+
+void print_all_alphabet_starting_from(char c)
+{
+	if (c > 'z')
+		return;
+	print_char(c);
+	print_all_alphabet_starting_from(c + 1);
+}
+
+void print_alphabet(void)
+{
+	print_all_alphabet_starting_from('a');
+	
+}
+
+int main(void)
+{
+	print_alphabet();
+	print_char(10);
+	return (0);
+}
