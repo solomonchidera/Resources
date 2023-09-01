64f57973e5ed99dd2deccbe0babcfbaf1a683b66
SolomonChidera <solochidera10@gmail.com>
2023-09-01 23:44:35 +0100

    usage of argc and argv

--- argv-argc/003.c
+++ argv-argc/003.c
@@ -2,17 +2,20 @@
 
 int main(int argc, char *argv[])
 {
+	int sum;
 	int count;
 
 	printf("This program was called with \"%s\".\n", argv[0]);
+	
 
 	if (argc > 1)
 	{
 		for (count = 1; count < argc; count++)
 		{
 			printf("argv[%d] = %s\n", count, argv[count]);
-
+			sum = += atoi(argv[i]);
 		}
+		printf("The sum of those argument are: %d\n", argv[i]);
 	}
 	else 
 	{
