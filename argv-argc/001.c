dc59edb2f1b6a3b71b5f32da55af78b7076162b2
SolomonChidera <solochidera10@gmail.com>
2023-09-01 18:17:56 +0100

    Printing the argument vector of argument count

---
+++ argv-argc/001.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+int main(int argc, char* argv[])
+{
+	int i;
+
+	printf("Number of argument: %d\n", argc)
+	for (i = 0; i < argc; ++i)
+	{
+		printf("Argument %d: %s", i, argv[i]);
+	}
+
+	return (0);
+}
