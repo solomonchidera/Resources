c49559af8bdd8fe2f9bb7d03740387b1857a4c1a
SolomonChidera <solochidera10@gmail.com>
2023-09-01 18:26:30 +0100

    usage of argc and argv

--- argv-argc/001.c
+++ argv-argc/001.c
@@ -4,7 +4,8 @@ int main(int argc, char* argv[])
 {
 	int i;
 
-	printf("Number of argument: %d\n", argc)
+	printf("Number of argument: %d\n", argc);
+
 	for (i = 0; i < argc; ++i)
 	{
 		printf("Argument %d: %s", i, argv[i]);
