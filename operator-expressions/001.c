dfe6d5d22bf120be9272959d957cb61a665b67ff
SolomonChidera <solochidera10@gmail.com>
2023-09-03 08:04:10 +0100

    code snippet from fresh2refresh.com

---
+++ operator-expressions/001.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+	int a=40,b=20, add,sub,mul,div,mod;
+	add = a+b;
+	sub = a-b;
+	mul = a*b;
+	div = a/b;
+	mod = a%b;
+	printf("Addition of a, b is : %d\n", add);
+	printf("Subtraction of a, b is : %d\n", sub);
+	printf("Multiplication of a, b is : %d\n", mul);
+	printf("Division of a, b is : %d\n", div);
+	printf("Modulus of a, b is : %d\n", mod);
+}
+
