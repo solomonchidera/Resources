f7f77a9fc086d68a0c5669df9bba27e9ec0aeb1b
SolomonChidera <solochidera10@gmail.com>
2023-09-04 16:29:41 +0100

    malloc

---
+++ malloc/malloc.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+	int n, i, *ptr;
+
+	printf("Enter the number of values\n");
+	scanf("%d", &n);
+
+	ptr = (int *)malloc(n * sizeof(int));
+
+	printf("Enter values\n");
+	for (i = 0; i < n; i++)
+	{
+		scanf("%d", (ptr + 1));
+	}
+
+	printf("The entered values are\n");
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", *(ptr + i));
+	}
+	free(ptr);
+}
