3ccfaa58acc94d54d53a4f4dd00530b6a33db9f1
SolomonChidera <solochidera10@gmail.com>
2023-09-05 08:44:16 +0100

    Adition of 3 digits of an array(malloc) and 'free'ing it with the free function

---
+++ malloc/free_mem.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+void m(int n0, int n1, int n2)
+{
+	int *t;
+	int sum;
+
+	t = malloc(sizeof(*t) * 3);
+	t[0] = n0;
+	t[1] = n1;
+	t[2] = n2;
+
+	sum = t[0] + t[1] + t[2];
+	printf("%d + %d + %d = %d\n", t[0], t[1], t[2], sum);
+	free(t);
+}
+
+int main(void)
+{
+	m(98, 402, -1024);
+	return (0);
+}
