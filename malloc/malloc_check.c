171118b80bb11ac6262f6c619c2e84ca1163c725
SolomonChidera <solochidera10@gmail.com>
2023-09-05 09:00:45 +0100

    This is an example on how to check the return value of malloc

---
+++ malloc/malloc_check.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <limits.h>
+
+int main(void)
+{
+	char *s;
+	int i;
+
+	i = 0;
+
+	while (1)
+	{
+		s = malloc(INT_MAX);
+		if (s == NULL)
+		{
+			printf("Can't allocate %d bytes (after %d calls)\n", INT_MAX, i);
+			return (1)
+		}
+		s[0] = 'H';
+		i++;
+	}
+	return (0);
+}
