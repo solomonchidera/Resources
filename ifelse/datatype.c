4bf426f1c9b7b2450e2ba97ae8fb446e7ac87c1f
SolomonChidera <solochidera10@gmail.com>
2023-09-06 08:37:57 +0100

    integer

---
+++ ifelse/datatype.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int score = 100;       // Signed integer
+	printf("%d \n", score);
+	unsigned int count = 42; // Unsigned integer
+	printf("%d \n", count);
+	short temperature = -5; // Short integer
+	printf("%d\n", temprature);
+	long population = 7000000000L; // Long integer
+	printf("%d\n", population);
+	long long very_large_number = 987654321012345678LL; // Long long integer
+	printf("%d\n", very_large_number);
+	return (0);
+}
