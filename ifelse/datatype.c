8b65c051b0c590c45d6409f1b530e4249dc2b735
SolomonChidera <solochidera10@gmail.com>
2023-09-06 08:41:35 +0100

    integers

--- ifelse/datatype.c
+++ ifelse/datatype.c
@@ -7,10 +7,10 @@ int main(void)
 	unsigned int count = 42; // Unsigned integer
 	printf("%d \n", count);
 	short temperature = -5; // Short integer
-	printf("%d\n", temprature);
+	printf("%d\n", temperature);
 	long population = 7000000000L; // Long integer
-	printf("%d\n", population);
+	printf("%ld\n", population);
 	long long very_large_number = 987654321012345678LL; // Long long integer
-	printf("%d\n", very_large_number);
+	printf("%lld\n", very_large_number);
 	return (0);
 }
