4d7935c26dbe28a5f2c45d8bdc78be8dceb20be6
SolomonChidera <solochidera10@gmail.com>
2023-09-07 06:27:05 +0100

    Arithmetic operators in C

---
+++ ifelse/001.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+int main(void)
+{
+	int a, b, c;
+	a = 10, b = 20;
+
+	c = a + b;
+	printf("a:%d + b:%d = %d\n", a, b, c);
+
+	c = a - b;
+	printf("a:%d - b:%d = %d\n", a, b, c);
+
+	c = a * b;
+	printf("a:%d * b:%d = %d\n", a, b, c);
+
+	c = a / b;
+	printf("a:%d / b:%d = %d\n", a, b, c);
+
+	c = a % b;
+	printf("a:%d % b:%d = %d\n", a, b, c);
+
+	c = a++;
+	printf("a++ will give us this output: %d", c);
+
+	c = b++;
+	printf("b++ will give us this output: %d", c);
+
+	return (0);
+
+}
