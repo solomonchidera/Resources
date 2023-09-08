649a7397d8e56b3f1ecba1e8a25503b11f1eea7c
SolomonChidera <solochidera10@gmail.com>
2023-09-08 12:08:42 +0100

    Function that adds two number(easy)

---
+++ Function/Deeper/add.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+
+int add(int a, int b)
+{
+	int result;
+	result = a + b;
+	return (result);
+
+}
+
+
+int main(void)
+{
+	int a, b;
+	a = 3, b = 10;
+	add(a, b);
+	return (0);
+
+}
