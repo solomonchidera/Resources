d1886a055144adfc31961103a14dfa5847f8dddf
SolomonChidera <solochidera10@gmail.com>
2023-09-08 14:41:02 +0100

    Factorial of a given number)

--- Function/Deeper/factorial.c
+++ Function/Deeper/factorial.c
@@ -2,16 +2,18 @@
 
 int factorial(int n)
 {
+	if (n == 0)
+		return (1);
+
 	n = n * (n - 1);
 	return (n);
 }
 
 int main(void)
 {
-	int i;
-	printf("Choose a number that you want to factorize ");
-	scanf("%d\n", &i);
-	factorial(i);
-	printf("Th factorial of %d is %d\n", i, factorial(i));
+	int i, fac;
+	i = 5;	
+	fac = factorial(i);
+	printf("Th factorial of %d is %d\n", i, fac);
 	return (0);
 }
