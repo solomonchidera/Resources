08a40459339e86dc01efcbbe7e00f0ca6a0f0754
SolomonChidera <solochidera10@gmail.com>
2023-09-10 21:14:11 +0100

    Adding level to the small calculator game

---
+++ Function/Deeper/playagain.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+
+void play_again()
+{
+	char *yes;
+	char *no;
+
+	yes = "yes";
+	no = "no";
+
+	printf("will you like to play again?");
+	scanf("%s", &play);
+
+
+	if (play == yes)
+
+
+}
