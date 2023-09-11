37a960450ae2cc5b02880dbfe78008f0a5a128bc
SolomonChidera <solochidera10@gmail.com>
2023-09-11 17:07:06 +0100

    Completing the function “new_user” to get a specific output

---
+++ Structure/exercise1.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+struct User
+{
+	char *name;
+	char *email;
+	int age;
+};
+
+struct User *new_user(char *name, char *email, int age)
+{
+	struct User *user;
+
+	return user;
+}
+
+int main(void)
+{
+	struct User *user;
+	user = new_user("Foo", "foo@hbtn.io", 98);
+	if (user == NULL)
+		return (1);
+
+	printf("User %s created !\n", user->name);
+	printf("His email is: %s\n", user->email);
+	printf("And he is %d years old\n", user->age);
+	return (0);
+}
