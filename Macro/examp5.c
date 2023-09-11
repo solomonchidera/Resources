5f9c5d708fc34c572acf3e2f25a68c2f979a0ec5
SolomonChidera <solochidera10@gmail.com>
2023-09-11 11:46:19 +0100

    complex macro usage

---
+++ Macro/examp5.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+#define BUFSIZE 1002
+#define TABLESIZE BUFSIZE
+
+int main(void)
+{
+	int num;
+	num = TABLESIZE;
+
+	printf("TABLESIZE %d\n", num);
+	printf("BUFSIZE %d\n", BUFSIZE);
+	return (0);
