49e8ca821d35b81b4a6ee0bc3132dcb185a0579c
SolomonChidera <solochidera10@gmail.com>
2023-09-11 16:56:58 +0100

    Example of struct usage (pointers to struct)

--- Structure/examp2.c
+++ Structure/examp2.c
@@ -10,11 +10,15 @@ struct User
 int main(void)
 {
 	struct User user;
+	struct User *ptr;
 	
-	user.name = "Foo Bar";
-	user.email = "foo@hbtn.io";
-	user.age = 98;
+	ptr = &user;
+
+	(*ptr).name "Foo Bar";
+	ptr->email = "foo@hbtn.io";
+	ptr->age = 98;
 	printf("sizeof User %lu\n", sizeof(struct User));
 	printf("sizeof user.name %lu\n", sizeof(user.name));
+	printf("Value of age is: %d\n", ptr->age);
 	return (0);
 }
