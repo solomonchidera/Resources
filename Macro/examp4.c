f257535406bcf566763fe5115c1f52f7ffab2f0e
SolomonChidera <solochidera10@gmail.com>
2023-09-11 11:34:01 +0100

    Macros does not apply to numbers only, you can also use it for creation of arrays

---
+++ Macro/examp4.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+#define NUMBERS 1, \
+		2, \
+		3, \
+		4
+int main(void)
+{
+	int x[] = {NUMBERS};
+	int i;
+
+	for (i = 0; i < x[i]; ++i)
+		printf("%d\n", x[i]);
+
+	return (0);
+}
