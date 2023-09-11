bbe706ede435242b372d2ae6c4955fc718dba94c
SolomonChidera <solochidera10@gmail.com>
2023-09-11 10:49:37 +0100

    Example of Macros in C

---
+++ Macro/examp1.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/* Defining a macro called MAX_VALUE*/
+#define MAX_VALUE 100
+
+int main(void)
+{
+	int num;
+	num = 75;
+	
+	if (num < MAX_VALUE)
+	{
+		printf("Num is less that MAX_VALUE\n");
+	}
+	return (0);
+
+}
