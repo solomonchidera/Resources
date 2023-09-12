f42dcb7ce31825b861c8bcb1027b15b426506941
SolomonChidera <solochidera10@gmail.com>
2023-09-12 18:28:38 +0100

    Header file

--- Structure/main.h
+++ Structure/main.h
@@ -1,5 +1,16 @@
 #ifndef MAIN_H
 #define MAIN_H
+
+struct solomon
+{
+	char *name;
+	char *email;
+	char *pet;
+	char *fun-fact;
+	int age;
+	float salary;
+};
+
 #include <stdio.h>
 #include <stdlib.h>
 #endif
