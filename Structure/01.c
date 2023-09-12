ed02feb772dd582f9f012b61bf3ee454c066d7ef
SolomonChidera <solochidera10@gmail.com>
2023-09-12 18:48:13 +0100

    Practicing on struct

--- Structure/01.c
+++ Structure/01.c
@@ -8,5 +8,11 @@ int main(void)
 	solomon.email = "solo@me.com";
 	solomon.pet = "lily";
 	solomon.salary = 0.00;
-	solomon.
+	solomon.fun-fact = "Love's coding in tears";
+
+	printf("My name is %s and his email address is %s\n", solomon.name, solomon.email);
+	printf("His pet name is %s and can you imagine that his salary is: %s\n", solomon.pet, solomon.salary);
+	printf("His fun fact %s\n", solomon.fun-fact);
+
+	return (0);
 }
