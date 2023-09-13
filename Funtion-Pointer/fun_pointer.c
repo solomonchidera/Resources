a6e6a36581a173eb2fb951faa98035de84b86b36
SolomonChidera <solochidera10@gmail.com>
2023-09-13 06:36:28 +0100

    Example of funtion pointers

---
+++ Funtion-Pointer/fun_pointer.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+int subs(int a, int b)
+{
+	return (a - b);
+}
+
+void Mathoperation(int x, int y, (*operation)(int, int))
+{
+	int result;
+	result = operation(x, y);
+	printf("Result: %d", result);
+}
+
+int main(void)
+{
+	Mathoperatin(5, 6, add);
+	Mathoperation(11, 3, subs);
+	return (0);
+}
