84d632e9146cd4db90132932944d138665a7568f
SolomonChidera <solochidera10@gmail.com>
2023-09-13 13:49:52 +0100

    Function that sorts an array of integers

---
+++ Funtion-Pointer/qsort.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+int value[] = {40, 10, 200, 90, 20, 25};
+
+int compare(const void *a, const void *b)
+{
+	return ( *(int*)a - *(int*)b );
+}
+
+int main(void)
+{
+	int n;
+	qsort (value, 6, sizeof(int), compare);
+
+	for (n = 0; n < 6; n++)
+	{
+		printf("%d\n", value[n])
+	}
+	return (0);
+}
