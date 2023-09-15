2b1eb1fab301a7e3b922f43d69a687a6122ebc6e
SolomonChidera <solochidera10@gmail.com>
2023-09-15 09:36:36 +0100

    Assigning a random value to array of integers

---
+++ Pointer/random.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(void)
+{
+	int v[100];
+
+	for (int i = 0; i < 100; ++i)
+		v[i] = rand() % 101;
+
+	printf("%d\n", v[42]);
+	printf("%d\n", *(v + 42));
+	printf("%d\n", 42[v]);
+	printf("%d\n", *(42 + v));
+
+	// WHAT? Murky area
+	printf("%c\n", "hello"[1]);
+	printf("%c\n", *("hello" + 1));
+	printf("%c\n", 1["hello"]);
+}
