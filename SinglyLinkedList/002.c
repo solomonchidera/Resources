36537a61315c117a226e6b2f03e2a82477fbf1f5
SolomonChidera <solochidera10@gmail.com>
2023-09-23 14:28:55 +0100

    Creating a single linked list with multiple nodes(adding the third node using two methods)

--- SinglyLinkedList/002.c
+++ SinglyLinkedList/002.c
@@ -17,7 +17,7 @@ int main(void)
 {
 	struct node *head;
 	struct node *node2;
-
+	struct node *node3;
 	head = (struct node *)malloc(sizeof(struct node));
 	if (head == NULL)
 	{
@@ -38,8 +38,21 @@ int main(void)
 	node2->next = NULL;
 	head->next = node2;
 
+	/*Adding the third node to the list (method 1)*/
+	node3 = malloc(sizeof(struct node));
+	if (node3 == NULL)
+	{
+		printf("Not enough memory to allocate\n");
+		exit(1);
+	}
+	node3->data = 22;
+	node3->next = NULL;
+	node2->next = node3;
+	
+
 	printf("head->data: %d\n"
-		"node2->data: %d\n", head->data, node2->data);
+		"node2->data: %d\n"
+		"node3->data: %d\n", head->data, node2->data, node3->data);
 
 	return (0);
 }
