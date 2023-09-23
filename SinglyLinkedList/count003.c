6bfd897400d6160ebdf2f1b0979ced1b5f1193f2
SolomonChidera <solochidera10@gmail.com>
2023-09-23 20:27:40 +0100

    Function that counts the nodes of a linked list

--- SinglyLinkedList/count003.c
+++ SinglyLinkedList/count003.c
@@ -3,13 +3,16 @@
 /**
  * Objective: counting the node in a list.
  * FileName: count003.c
+ * Lang: C
+ * Author: SolomonChidera
  */
 
 void count_no_nodes(struct node *head)
 {
 	int count;
 	struct node *ptr;
-	count = 0, ptr = NULL;
+	count = 0;
+	ptr = NULL;
 
 	if (head == NULL)
 	{
