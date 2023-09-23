71600f617e1d49a96c0f94c74875f1ad48476c55
SolomonChidera <solochidera10@gmail.com>
2023-09-23 20:22:31 +0100

    Function that counts the nodes of a linked list

---
+++ SinglyLinkedList/count003.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * Objective: counting the node in a list.
+ * FileName: count003.c
+ */
+
+void count_no_nodes(struct node *head)
+{
+	int count;
+	struct node *ptr;
+	count = 0, ptr = NULL;
+
+	if (head == NULL)
+	{
+		printf("List is empty\n");
+		exit(1);
+	}
+
+	ptr = head;
+
+	while (ptr != NULL)
+	{
+		count++;
+		ptr = ptr->next;
+	}
+	printf("%d\n", count);
+}
