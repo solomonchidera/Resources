91cbfdc6f83c45f5d9c0778be8225fa998c6ea88
SolomonChidera <solochidera10@gmail.com>
2023-10-03 22:35:40 +0100

    Understanding fgets() function

---
+++ Shell/fgets.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+int main(void)
+{
+	FILE *file;
+	char str[70];
+
+	file = fopen("loremIpsum.txt", "r");
+	if (NULL == file)
+	{
+		perror("Dude watch your back :)\n");
+		return (-1);
+	}
+
+	if (fgets(str, 70, file) != NULL)
+	{
+		printf("%s\n" &str);
+	}
+
+	return (0);
+}
