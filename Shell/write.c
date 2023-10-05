92331a2a656dd0f4e17f02bf1e334e8419165850
Solomon.Chidera <116091273+SolomonChidera@users.noreply.github.com>
2023-10-05 21:55:40 +0100

    Update write.c

--- Shell/write.c
+++ Shell/write.c
@@ -1,5 +1,8 @@
 #include "main.h"
-
+/**
+ * print - function that was built to be used as string printer
+ * Return: coming soon
+ */
 int print(const char *string)
 {
 	return(write(1, &string, strlen(string)));
