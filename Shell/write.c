94df93b2d7b6911cda6aea682c1f6e6918d2ea2c
SolomonChidera <solochidera10@gmail.com>
2023-10-06 14:58:02 +0100

    Just checking somtin

--- Shell/write.c
+++ Shell/write.c
@@ -8,3 +8,14 @@ int print(const char *string)
 {
 	return(write(1, &string, strlen(string)));
 }
+
+
+int main(void)
+{
+	char test[9] = "Moniaar";
+
+	print(test);
+	putchar(10);
+
+	return (0);
+}
