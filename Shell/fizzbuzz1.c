9f522a1360ede73b73c10e30bdf11e7ebf590f21
SolomonChidera <solochidera10@gmail.com>
2023-10-07 07:21:53 +0100

    Fizzbuzz challenge using C [first solution]

--- Shell/fizzbuzz1.c
+++ Shell/fizzbuzz1.c
@@ -7,11 +7,11 @@ int main(void)
 
 	for (; i <= 100; i++)
 	{
-		if (i / 3 && i / 5)
+		if (i % 3 == 0 && i % 5 == 0)
 			printf("FizzBuzz");
-		else if (i / 3)
+		else if (i % 3 == 0)
 			printf("Fizz");
-		else if (i / 5)
+		else if (i % 5 == 0)
 			printf("Buzz");
 		else
 			printf("%d", i);
