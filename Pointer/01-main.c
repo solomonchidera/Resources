6555434095115011d0eec7067433342befcb4dc8
SolomonChidera <solochidera10@gmail.com>
2024-01-06 17:32:42 +0100

    I edited this code in #10 and hope this will work I destroyed this code I don't care

--- Pointer/01-main.c
+++ Pointer/01-main.c
@@ -7,9 +7,11 @@ int main(void)
 	int *p;
 
 	p = &var;
+	*p = 159;
 
 	printf("The address of var is %p\n", &var);
 	printf("The value of '*p' is %p\n", p);
+	printf("I just remembered my old laptop and am crying %p\n", *p);
 
 	return (0);
 }
