e26a2161a0235bfb66e1af6e9ba2eca4ef53aa26
solomonchidera <solochidera10@gmail.com>
2024-06-10 16:26:38 +0100

    asyncio practice

---
+++ Tutorial/async.py
@@ -0,0 +1,18 @@
+import asyncio
+
+
+async def greet(name):
+    print(f"Hello, {name}")
+    await asyncio.sleep(1)
+    print(f"How are you doing, {name}")
+
+
+async def main():
+    await asyncio.gather(
+        greet("solomon"),
+        greet("moniaar"),
+        greet("yahia")
+    )
+
+
+asyncio.run(main)
